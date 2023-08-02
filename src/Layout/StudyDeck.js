c27ea6b356cc1963c8f91ca0b358f1d79faa3843
MTaslam2000 <taiyab.aslam728@gmail.com>
2023-08-02 13:31:21 -0500

    my study page is fully functional

--- src/Layout/StudyDeck.js
+++ src/Layout/StudyDeck.js
@@ -8,6 +8,7 @@ function StudyDeck() {
   const [flipFront, setFlipFront] = useState(true);
   const { deckId } = useParams();
   const [currentIndex, setCurrentIndex] = useState(0); // Track the current card index
+  const [isFinished, setIsFinished] = useState(false); // Track if all cards have been shown
 
   const flipCardHandler = () => setFlipFront(!flipFront);
 
@@ -28,18 +29,78 @@ function StudyDeck() {
     setFlipFront(true);
   };
 
-  if (cards.length === 0) {
-    return <div>Loading...</div>;
+  const restartHandler = () => {
+    // Reset the current index and the card flip state to start over
+    setCurrentIndex(0);
+    setFlipFront(true);
+    setIsFinished(false);
+  };
+
+  if (cards.length <= 2) {
+    return (
+      <>
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link to="/">
+                <span className="oi oi-home"></span> Home
+              </Link>
+            </li>
+            <li className="breadcrumb-item">
+              <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Study
+            </li>
+          </ol>
+        </nav>
+        <h1>{deck.name}: Study</h1>
+        <h2>Not Enough Cards</h2>
+        <p>
+          You need at least 3 cards to study. There are {cards.length} cards in
+          this deck.
+        </p>
+        <Link
+          to={`/decks/${deck.id}/cards/new`}
+          className="btn btn-primary mr-2"
+        >
+          <span className="oi oi-pencil"></span>
+          Add more
+        </Link>
+      </>
+    );
   }
 
   return (
     <div>
-      You are studying this deck
+      <h1>{deck.name}: Study</h1>
       <div>
         {flipFront ? (
-          <div>{cards[currentIndex].front}</div>
+          <div className="card mb-3">
+            <div className="card-body">
+              <div className="row">
+                <div className="col">
+                  <h5 className="card-title">
+                    Card {currentIndex + 1} of {cards.length}
+                  </h5>
+                  <div>{cards[currentIndex].front}</div>
+                </div>
+              </div>
+            </div>
+          </div>
         ) : (
-          <div>{cards[currentIndex].back}</div>
+          <div className="card mb-3">
+            <div className="card-body">
+              <div className="row">
+                <div className="col">
+                  <h5 className="card-title">
+                    Card {currentIndex + 1} of {cards.length}
+                  </h5>
+                  <div>{cards[currentIndex].back}</div>
+                </div>
+              </div>
+            </div>
+          </div>
         )}
         <button
           type="button"
@@ -48,13 +109,27 @@ function StudyDeck() {
         >
           Flip
         </button>
-        <button
-          type="button"
-          className="btn btn-primary mr-2"
-          onClick={nextCardHandler}
-        >
-          Next
-        </button>
+        {currentIndex < cards.length - 1 && !flipFront && (
+          <button
+            type="button"
+            className="btn btn-primary mr-2"
+            onClick={nextCardHandler}
+          >
+            Next
+          </button>
+        )}
+        {currentIndex === cards.length - 1 && !flipFront && (
+          <div>
+            <p>Congratulations! You have finished studying all the cards.</p>
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={restartHandler}
+            >
+              Restart
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
